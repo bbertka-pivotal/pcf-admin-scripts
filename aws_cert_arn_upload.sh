2b19d68d2ef95c3df05738beff5e3727ee18c438
bbertka-pivotal <bbertka@pivotal.io>
2016-09-07 22:44:27 -0700

    Added cert arn upload script

---
+++ aws_cert_arn_upload.sh
@@ -0,0 +1,8 @@
+#1/usr/sh
+
+openssl genrsa -out my-private-key.pem 2048
+openssl req -sha256 -new -key my-private-key.pem -out csr.pem -config openssl.cnf
+openssl x509 -req -days 365 -in csr.pem -signkey my-private-key.pem -out my-certificate.pem
+openssl rsa -in my-private-key -outform PEM
+aws iam upload-server-certificate --server-certificate-name my-server-certificate --certificate-body file://my-certificate.pem --private-key file://my-private-key.pem
+aws iam get-server-certificate --server-certificate-name my-server-certificate
