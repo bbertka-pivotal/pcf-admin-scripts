4ec37fbe45279ac774db556baae8c5ec972ddfab
Ubuntu <ubuntu@ip-10-0-0-62.us-west-2.compute.internal>
2016-09-09 04:48:21 +0000

    Removed unecessary uaac

--- pcf-stop-script.sh
+++ pcf-stop-script.sh
@@ -3,7 +3,6 @@
 export RAILS_ENV=production
 
 alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
-alias uaac='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/uaac/Gemfile bundle exec uaac'
 
 bosh stop cloud_controller
 bosh stop cloud_controller_worker
