07371a802c4a3246a682d8b2fc556c18cf1fb661
Ubuntu <ubuntu@ip-10-0-0-156.us-west-2.compute.internal>
2016-09-27 18:30:17 +0000

    Added tile and pcf utility installer

---
+++ tile_generator_install.sh
@@ -0,0 +1,30 @@
+#/bin/sh
+
+#Installs Tile Generator
+
+# note, do not run this as Sudo
+# be sure you install python dependencies as user
+
+
+git clone https://github.com/cf-platform-eng/tile-generator.git
+cd tile-generator
+
+# create meatdata for using the PCF Ops Man Utility
+touch metadata
+echo "---" >> metadata
+echo "opsmgr:" >> metadata
+echo "    url: https://opsman.your-company.com" >> metadata
+echo "    username: admin" >> metadata
+echo "    password: password" >> metadata
+
+# if this fails, make sure to install everything as local user
+pip install -r requirements.txt
+
+echo "export PATH=`pwd`/bin:$PATH" >> ~/.profile
+source ~/.profile
+
+# test the utilities
+which tile
+which pcf
+
+echo "Done.  Please update ./metadata with your ops man creds"
