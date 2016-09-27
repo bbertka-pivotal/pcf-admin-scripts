d142d3b1a4d4db0e124ed98ac2090b2fcccd65ad
Ubuntu <ubuntu@ip-10-0-0-156.us-west-2.compute.internal>
2016-09-27 18:33:09 +0000

    Fixed command typo

---
+++ docker_engine_install.sh
@@ -0,0 +1,13 @@
+#!/usr/sh
+
+# This Script installs Docker
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main"  > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get install docker-engine
+sudo service docker start
