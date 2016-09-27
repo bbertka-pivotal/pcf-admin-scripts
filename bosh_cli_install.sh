a0572fecbad5804e164e6c3a30f9150d25f03e31
bbertka-pivotal <bbertka@pivotal.io>
2016-09-27 10:56:31 -0700

    Added bosh cli installer

---
+++ bosh_cli_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Installs the latest BOSH CLI
+
+sudo apt-get install build-essential ruby ruby-dev libxml2-dev libsqlite3-dev libxslt1-dev libpq-dev libmysqlclient-dev zlib1g-dev
+gem install bosh_cli --no-ri --no-rdoc
+which bosh
+bosh status
