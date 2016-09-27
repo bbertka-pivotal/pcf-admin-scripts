98bc0e0634ae97ac9b92e708ecffdf30dba336ec
Ubuntu <ubuntu@ip-10-0-0-156.us-west-2.compute.internal>
2016-09-27 18:17:18 +0000

    Added login

--- bosh_target_aws.sh
+++ bosh_target_aws.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-export RAILS_ENV=production
+# Targets BOSH Director
 
+# comment following three lines of you have installed Bosh CLI or UAAC CLI seperatley
+export RAILS_ENV=production
 alias bosh='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
 alias uaac='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/uaac/Gemfile bundle exec uaac'
 
 uaac target --ca-cert /var/tempest/workspaces/default/root_ca_certificate https://10.0.16.10:8443
-bosh --ca-cert /var/tempest/workspaces/default/root_ca_certificate target 10.0.16.10
+bosh --ca-cert /var/tempest/workspaces/default/root_ca_certificate target
+bosh login
+
+# tests bosh
+bosh releases
+bosh deployments
 bosh status
