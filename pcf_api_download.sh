a59bb87595d62d0b5c1f96df2152855af3813887
Ubuntu <ubuntu@ip-10-0-0-87.us-west-2.compute.internal>
2016-09-29 23:12:43 +0000

    Fixed env var issue

--- pcf_api_download.sh
+++ pcf_api_download.sh
@@ -3,22 +3,22 @@
 # Downloads a product from network.pivotal.io and uploads to ops manager"
 
 # update these for a new product
-PRODUCT_NAME="cf-1.8.3-build.1.pivotal"
-API_DOWNLOAD_LINK="https://network.pivotal.io/api/v2/products/elastic-runtime/releases/2324/product_files/7227/download"
+PRODUCT_NAME="cf-1.8.2-build.3.pivotal"
+API_DOWNLOAD_LINK="https://network.pivotal.io/api/v2/products/elastic-runtime/releases/2302/product_files/7059/download"
 
 # These dont need to change
-PIVOTAL_NETWORK_API_TOKEN="relace with your network.pivotal.io api key"
+PIVOTAL_NETWORK_API_TOKEN="your-pivnet-api-token"
 OPSMAN="https://ops.yourcompany.com"
 
 # credentials for your ops man admin
-OPSPASS="changeme"
-OPSUSER="admin"
+OPSPASS="ops web password"
+OPSUSER="ops web user"
 
 # Download product as FILENAME
 wget -O $PRODUCT_NAME --post-data="" --header="Authorization: Token ${PIVOTAL_NETWORK_API_TOKEN}" $API_DOWNLOAD_LINK
 
 # Get ops man uaa access token
-uaac target $OPSMAN/uaa
+uaac target $OPSMAN/uaa --skip-ssl-validation
 uaac token owner get opsman $OPSUSER -s "" -p $OPSPASS
 UAA_ACCESS_TOKEN="$(uaac context | grep -o -P '(?<=access_token: ).*')"
 
@@ -26,5 +26,5 @@ UAA_ACCESS_TOKEN="$(uaac context | grep -o -P '(?<=access_token: ).*')"
 curl -k "${OPSMAN}/api/v0/available_products" \
     -X POST \
     -H "Authorization: Bearer ${UAA_ACCESS_TOKEN}" \
-    -F "product[file]=@./${FILENAME}" \
+    -F "product[file]=@./${PRODUCT_NAME}" \
     --progress-bar --verbose -o pcf_api_upload.txt
