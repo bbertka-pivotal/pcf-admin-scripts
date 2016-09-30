c92627a9ae6181c5c227b384cbf4c4664c02698a
Ubuntu <ubuntu@ip-10-0-0-102.us-west-2.compute.internal>
2016-09-30 05:03:49 +0000

    Added uaac cli

--- bosh_cli_install.sh
+++ bosh_cli_install.sh
@@ -2,7 +2,10 @@
 
 # Installs the latest BOSH CLI
 
+
+
 sudo apt-get install build-essential ruby ruby-dev libxml2-dev libsqlite3-dev libxslt1-dev libpq-dev libmysqlclient-dev zlib1g-dev
+gem install cf-uaac
 gem install bosh_cli --no-ri --no-rdoc
 which bosh
 bosh status
